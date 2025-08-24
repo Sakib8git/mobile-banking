95c9f042b45d3c2babdb7d046f6b67c80e179ce3
Sakib8git <sakib30278@gnail.com>
2025-08-24 20:44:10 +0600

    toggling- commit

--- js/home.js
+++ js/home.js
@@ -1,20 +1,50 @@
-document.getElementById("add-money-op").addEventListener("click", function () {
-  const addMoneySec = document.getElementById("add-money-sec");
-  const outMoneySec = document.getElementById("out-money-sec");
+const pin = 1122;
+// !function get input values for num(Pin and amount)
+function getInputValueNumber(id) {
+  const inputFildeValue = parseInt(document.getElementById(id).value);
+  return inputFildeValue;
+}
 
-  addMoneySec.style.display = "block";
-  outMoneySec.style.display = "none";
-});
+// !function get input values for String(Account and agent num)
+function getInputValueSrt(id) {
+  const inputSrtValue = document.getElementById(id).value;
+  return inputSrtValue;
+}
 
-// addd money
+// !function get  innreText
+function getInnerText(id) {
+  const insideText = parseInt(document.getElementById(id).innerText);
+  return insideText;
+}
+// !function to set  new innreText
+function getInnerNewText(value) {
+  const newBalance = document.getElementById("my-balance");
+  newBalance.innerText = value;
+  console.log(newBalance);
+  return newBalance;
+}
+// !function to toggling
+function handleToggler(id) {
+  const forms = document.getElementsByClassName("form");
+  for (const item of forms) {
+    item.style.display = "none";
+  }
+  document.getElementById(id).style.display = "block";
+}
+
+// !function to tOGGLING
+
+//note: *Shared code end
+
+// fixme:addd money
 document.getElementById("add-btn").addEventListener("click", function (event) {
   event.preventDefault();
-  let myBalance = parseInt(document.getElementById("my-balance").innerText);
-  const accountNum = document.getElementById("account-num").value;
-  const addAmount = parseInt(document.getElementById("add-amount").value);
-  const pinNum = parseInt(document.getElementById("pin-num").value);
-  const pin = 1122;
-  // console.log(addAmount);
+  let myBalance = getInnerText("my-balance");
+  const bank = getInputValueSrt("bank");
+  const accountNum = getInputValueSrt("account-num");
+  const addAmount = getInputValueNumber("add-amount");
+  const pinNum = getInputValueNumber("pin-num");
+
   if (accountNum.length < 11) {
     alert("Please input correct account number");
     return;
@@ -24,25 +54,50 @@ document.getElementById("add-btn").addEventListener("click", function (event) {
     return;
   }
   const newBalance = myBalance + addAmount;
-  document.getElementById("my-balance").innerText = newBalance;
+  getInnerNewText(newBalance);
+});
+// *_________toggler
+
+document.getElementById("add-money-op").addEventListener("click", function () {
+  handleToggler("add-money-sec");
 });
 
-// cash out part
 document.getElementById("out-money-op").addEventListener("click", function () {
-  const addMoneySec = document.getElementById("add-money-sec");
-  const outMoneySec = document.getElementById("out-money-sec");
-
-  addMoneySec.style.display = "none";
-  outMoneySec.style.display = "block";
+  handleToggler("out-money-sec");
 });
+document
+  .getElementById("transfer-money-op")
+  .addEventListener("click", function () {
+    handleToggler("transfer-money-sec");
+  });
+
+document
+  .getElementById("bonus-money-op")
+  .addEventListener("click", function () {
+    handleToggler("bonus-money-sec");
+  });
+
+document.getElementById("pay-money-op").addEventListener("click", function () {
+  handleToggler("pay-money-sec");
+});
+
+document
+  .getElementById("transactions-money-op")
+  .addEventListener("click", function () {
+    handleToggler("transactions-money-sec");
+  });
+
+// !_________toggler end
+
+// fixme:cash out part
 
 document.getElementById("out-btn").addEventListener("click", function (event) {
   event.preventDefault();
-  let myBalance = parseInt(document.getElementById("my-balance").innerText);
-  const agenttNum = document.getElementById("agent-num").value;
-  const outAmount = parseInt(document.getElementById("out-amount").value);
-  const pinNum = parseInt(document.getElementById("pin-num-out").value);
-  const pin = 1122;
+  let myBalance = getInnerText("my-balance");
+  const agenttNum = getInputValueSrt("agent-num");
+  const outAmount = getInputValueNumber("out-amount");
+  const pinNum = getInputValueNumber("pin-num-out");
+
   // console.log(addAmount);
   if (agenttNum.length < 11) {
     alert("Please input correct agent number");
@@ -53,5 +108,5 @@ document.getElementById("out-btn").addEventListener("click", function (event) {
     return;
   }
   const newBalance = myBalance - outAmount;
-  document.getElementById("my-balance").innerText = newBalance;
+  getInnerNewText(newBalance);
 });
